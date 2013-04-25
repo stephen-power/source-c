c66072b6525bfb3106c5f73a3bd23df4567f0d6b
stephen-power <me@server.com>
2013-04-25 12:35:23 +1000

    more work on pronto.c

--- pronto.c
+++ pronto.c
@@ -1,19 +1,60 @@
-//gcc charArray.c -o ./bin/charArray
+//gcc pronto.c -o ./bin/pronto
 #include <stdio.h>
+#include <string.h>
 
 char testInArr[] = "+0,73,0,12,F,A,6,A,6,16,6,A,6,A,6,1C,6,16,6,A,6,A,6,16,6,16,6,10,6,16,6,A,6,A,6,A,6,10,6,E12-"; // simulate USART data in
-char inputArr[500];
+unsigned int nbrCommas;		// on the atmega this number will be determined as the input stream is read. 
+char inputArr[500];		//this will hold what comes in from USART
 int inputArrLen;    //will be the number of chars loaded into the arrray (one bigger than the end index)
 int testInArrSize = sizeof(testInArr);
 
+unsigned int prontoIntArr[500];
+int prontoIntArr_size;
  
 int main() {
     printf("charArray\n");
-	printCharArray();
-	simulateReadingFromUSART();
+	loadInputArray();
+	//printCharArray();
+	//simulateReadingFromUSART();
 
     return 0;
 }
+
+loadInputArray()
+{
+	nbrCommas = 0;
+	printf("loadInputArray\n");
+
+    int inSize = testInArrSize - 1;
+	printf("i=%d\n", inSize); 
+    
+	int i2 = 0;
+	int i;
+    for(i = 0; i < inSize; i++)
+    {    // load the test array into inputArr; this is only done to simulate USART input
+		// and remove '+' and '-' chars
+        //printf("char=%c", testInArr[i]);
+		if (testInArr[i] == '+' || testInArr[i] == '-'); //drop these
+		else
+			{
+			inputArr[i2] = testInArr[i];
+			if (inputArr[i2] == ',') nbrCommas++;
+			i2++;
+			}
+    }
+	printf("nbrCommas=%d\n", nbrCommas);   
+    printf("inputArr<%s>\n", inputArr);
+
+    //printf("inputArrLen=%d\n", inputArrLen);
+/*	printf("<");
+	for (i = inSize; i > -1; i--) 	
+	{
+		printf("%c", inputArr[i]);
+		//printf("i=%d", i);
+	}
+	printf(">\n");
+*/
+} 
 
 printCharArray()
 {
@@ -27,7 +68,11 @@ printCharArray()
 }
 
 simulateReadingFromUSART()
-{//  instead of reading from USART we will read from testInArr
+{
+	/* this routine will read the charArray and convert the chars to ints and remove the comma's.
+	the array will be read backwards to assist in the conversion process
+	testInArr[] holds the input data as comma delimited hex chars  which need to be converted.
+	*/ 
 	int inSize = testInArrSize -2;
 	printf("i=%d\n", inSize); 
 	
@@ -40,4 +85,11 @@ simulateReadingFromUSART()
 	inputArrLen = i -1;	// save the length
 	printf("inputArr<%s>\n", inputArr); 	
 	printf("inputArrLen=%d\n", inputArrLen);
+
+	char *pos = strchr(testInArr, 'u');
+	if (pos)
+    	printf("Character 'u' found at position %d.\n", pos - testInArr);
+  	else
+    	printf("Character 'u' not found.\n");
+
 }
