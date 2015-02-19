5c48e284954c722c9155c03aa740b6cd647af15c
stephen-power <me@server.com>
2015-02-19 20:26:49 +1100

    working on isBitOn

--- bitOperations.c
+++ bitOperations.c
@@ -5,37 +5,40 @@ bool isBitOn (int, unsigned char);
 void printCharToBinaryString (char);
 
 int main() {
-    printf("---bitOperations start---\n");
+//    printf("---bitOperations start---\n");
 
 	unsigned char testByte = 'a';
 //	printCharToBinaryString (testByte);
 	
 	int bitNumber = 3;
-	isBitOn(bitNumber, 	testByte);
+	bool bitTest = isBitOn(bitNumber, 	testByte);
+	printf("bitTest=\n");
 
 
-    printf("---bitOperations end---\n");
+//    printf("---bitOperations end---\n");
     return 0;
 }
  
 bool isBitOn (int bitNumberToTest, unsigned char inByte){
-	printf("-isBitOn\n");	
+//	printf("-isBitOn\n");	
 	// bits are numbered right to left decending starting from 8.  e.g  8->1 
-	printf("bitNumberToTest=%d    inByte=%x\n", bitNumberToTest, inByte);	
+	printf("inByte=");	
 	printCharToBinaryString (inByte);
-	
 	int x;
 	unsigned char mask = 0b00000001;
 
-	for(x = 1; x != bitNumberToTest; x++){  // shift the "on" bit to the correct postion
-			printf("x=%d\n",x);
+	for(x = 1; x < bitNumberToTest; x++){  // shift the "on" bit to the correct postion
+			mask = mask << 1;	
 	}
-	
+	// mask will now be in the correct bit position	
+	printf("mask  =");	
+	printCharToBinaryString (mask);
+
 	return true;
 }
 
 void printCharToBinaryString (char charIn){
-	printf("-printCharToBinaryString\n");	
+//	printf("-printCharToBinaryString\n");	
 	int onOff = 0;
 	unsigned char mask = 0b10000000;
 	
