ac5a75d181a77b381525e1363e90a2ea6020dc98
stephen-power <me@server.com>
2015-02-20 21:08:50 +1100

    start function setTapsOnAsString

--- bitOperations.c
+++ bitOperations.c
@@ -1,8 +1,21 @@
 //gcc bitOperations.c -o ./bin/bitOperations
 #include <stdio.h>
 #include <stdbool.h>
+//******************************************************************************
+//  GLOBAL VARIABLES                                                           *
+//******************************************************************************
+char tapsOnAsString[3] = "x,x";   // length 3.  e.g "2,6" or "5  " or "   "
+
+//******************************************************************************
+//  FUNCTION DEFINITIONS                                                       *
+//******************************************************************************
 bool isBitOn (int, unsigned char);
 void printCharToBinaryString (char);
+void setTapsOnAsString(unsigned char);
+
+//******************************************************************************
+//  MAIN                                                                       *
+//******************************************************************************
 
 int main() {
 	unsigned char testByte = 'a';
@@ -13,9 +26,32 @@ int main() {
 	printf(bitTest ? "true" : "false");
 	printf("\n");
 
+	tapsOnAsString[2] = 'y';
+	printf("tapsOnAsString=%s\n", tapsOnAsString);
+	setTapsOnAsString(testByte);
+
+
     return 0;
 }
- 
+//******************************************************************************
+//  FUNCTIONS                                                                  *
+//******************************************************************************
+void setTapsOnAsString(unsigned char inByte){
+	int tapsOnAsStringIDX = 0;
+	tapsOnAsString[0] = ' ';
+	tapsOnAsString[1] = ' ';
+	tapsOnAsString[2] = ' ';
+
+	int bitPosition;
+	for(bitPosition = 1; bitPosition < 7; bitPosition++){  // only look at the first 6 bits
+		if(isBitOn(bitPosition, inByte)){
+			//printf("true");
+					
+		}
+	}
+ 	
+}
+//******************************************************************************
 bool isBitOn (int bitPositionToTest, unsigned char inByte){
 	// bits are numbered right to left decending starting from 8.  e.g  8->1 
 	printf("inByte=");	
@@ -35,7 +71,7 @@ bool isBitOn (int bitPositionToTest, unsigned char inByte){
 	}
 	return true;
 }
-
+//******************************************************************************
 void printCharToBinaryString (char charIn){
 	unsigned char mask = 0b10000000;
 	int x;
@@ -52,5 +88,6 @@ void printCharToBinaryString (char charIn){
 	}
 	printf("\n");	
 }
+//******************************************************************************
   
 
